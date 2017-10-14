eb30ce09eba17508edfea931e27c1c199a8b6bc3
Vaibhav Laghane <vaibhavlaghane@gmail.com>
2017-10-14 13:10:07 -0700

    play video on the controller view

--- DrillBaseB/DrillBase/Resources/Video.swift
+++ DrillBaseB/DrillBase/Resources/Video.swift
@@ -9,7 +9,34 @@
 import UIKit
 import CoreVideo
 import AVKit
+import AVFoundation
 
 class Video: NSObject {
 
+    
+    func videoPlayer()->AVPlayerViewController{
+ 
+        let playerViewController = AVPlayerViewController()
+ 
+    
+        
+        let videoURLWithPath = "/Drillbase/Resources/Catching-Framing.mov"
+        let videoURL = NSURL(string: videoURLWithPath)
+        playerViewController.player = AVPlayer.init(url: videoURL as! URL) as AVPlayer
+        
+        return playerViewController
+    }
+    
+    
+      func player()->AVPlayerViewController?{
+        guard let path = Bundle.main.path(forResource: "Catching-Framing", ofType:"mov") else {
+            debugPrint("video.m4v not found")
+            return nil
+        }
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        let playerController = AVPlayerViewController()
+        playerController.player = player
+       return playerController
+    }
+    
 }
--- DrillBaseB/DrillBase/SportsDrillsCategory/HomePage/HomePage.swift
+++ DrillBaseB/DrillBase/SportsDrillsCategory/HomePage/HomePage.swift
@@ -7,25 +7,83 @@
 //
 
 import UIKit
+import MediaPlayer
+import CoreMedia
 
 class HomePage: UIViewController {
 
     @IBOutlet weak var navBar: UINavigationBar!
-    
     @IBOutlet weak var toobar: UITabBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+
     }
 
+    
+    func loadVideo(){
+        
+////        var fileNamepath =
+////    
+////        NSString  *fileNamePath = mVideo;
+////        NSArray   *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory,NSUserDomainMask, YES);
+////        NSString  *documentsDirectory = [paths  objectAtIndex:0];
+////        NSString  *oldappSettingsPath = [documentsDirectory stringByAppendingPathComponent:fileNamePath];
+//      //  var path:URL  =
+////        self.mPlayer = [[MPMoviePlayerController alloc] initWithContentURL:path];
+//        
+//        //let playerViewController = AVPlayerViewController()
+//        
+//        let videoURLWithPath = "/Resources/Catching-Framing.mov"
+//        let videoURL = NSURL(string: videoURLWithPath)
+//      //  playerViewController.player = AVPlayer.init(url: videoURL as! URL) as AVPlayer
+//        
+//        
+//        var mPlayer = MPMoviePlayerController(contentURL: videoURL as URL!)
+//        mPlayer.controlStyle = .fullscreen
+//        mPlayer.fullscreen = YES;
+//        mPlayer.scalingMode = .fill//  MPMovieScalingModeFill;
+//        [[self.mPlayer view] setFrame: CGRectMake(0, 0, 480, 320)];
+//        [self.view addSubview:[self.mPlayer view]];
+//        [self.mPlayer prepareToPlay];
+//        [self.mPlayer play];
+//        
+     
+        let playVideo = Video()
+       if  let player = playVideo.player()// playVideo.videoPlayer()
+       {
+        self.present(player, animated: true) {
+            player.player!.play()
+        }
+        }
+    }
+    
+    
+
+    override func viewDidAppear(_ animated: Bool) {
+        
+        self.loadVideo()
+    }
+    
+    override func viewDidLayoutSubviews() {
+       // <#code#>
+    }
+    
+    override func viewWillLayoutSubviews() {
+       // <#code#>
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+       // <#code#>
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
     /*
     // MARK: - Navigation
 
