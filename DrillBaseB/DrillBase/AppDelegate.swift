96f1997ea53abb6637d1e1fe27aa9e9fe06fb454
Vaibhav Laghane <vaibhavlaghane@gmail.com>
2018-04-14 11:05:05 -0700

    FB SDK and Pods

--- DrillBaseB/DrillBase/AppDelegate.swift
+++ DrillBaseB/DrillBase/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FBSDKCoreKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions);
+        
         return true
     }
 
@@ -41,6 +44,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        let handled = FBSDKApplicationDelegate.sharedInstance().application(app , open: url, sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String, annotation: options[UIApplicationOpenURLOptionsKey.annotation])
+        
+        return handled;
+    }
+    
+ 
 }
 
--- DrillBaseB/DrillBase/ViewController.swift
+++ DrillBaseB/DrillBase/ViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
-    
     
     @IBOutlet weak var loginLabel: UIButton!
     @IBOutlet weak var registerWithFacebookLabel: UILabel!
@@ -18,6 +19,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //self.degreeOfArray(arr: [5,1,2,2,3,1])
+         // self.degreeOfArray(arr: [6,1,1,2,1,2,2])
+       // self.degreeOfArray(arr: [6,1,1,1,2,2,3,3,4,2])
+        let loginButton = FBSDKLoginButton() ;
+        // Optional: Place the button in the center of your view.
+        loginButton.center = self.view.center;
+        self.view.addSubview(loginButton) ;
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -47,7 +56,9 @@ class ViewController: UIViewController {
 
     
     @IBAction func loginButtonClicked(_ sender: Any) {
-        
+//        if (FBSDKAccessToken().currentAccessToken) {
+//            // User is logged in, do work such as go to next view controller.
+//        }
 //        print("login button clicked ")
 ////        let baseTabBarController = storyboard?.instantiateViewController(withIdentifier: "BaseTabBarController") as! BaseTabBarController
 ////        self.present(baseTabBarController, animated:true , completion: nil)
@@ -63,11 +74,87 @@ class ViewController: UIViewController {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
         
-//        if segue.identifier == "showHomePage" {
-//            //let dvc = segue.destination as! HomePage
-//           // dvc.delegate = self
-//        }
+        if segue.identifier == "showHomePage" {
+            //let dvc = segue.destination as! HomePage
+           // dvc.delegate = self
+        }
     }
+    
+    
+    /*
+    func degreeOfArray(arr: [Int]) -> Int {
+        // Write your code here.
+        if(arr.count == 0){return 0}
+        if(arr.count == 1){return 1}
+        var mapNumOccurences = [Int: Int ]()
+        var currMax = 0
+        var currMaxNum = 0
+        for i in 0...arr.count - 1{
+            if(mapNumOccurences[arr[i]] == nil ){
+                mapNumOccurences[arr[i]] = 0
+            }
+            mapNumOccurences[arr[i]] =  mapNumOccurences[arr[i]]! + 1 ;
+            if(mapNumOccurences[arr[i]]! > currMax )
+            {
+                currMax  = mapNumOccurences[arr[i]]!
+                currMaxNum = arr[i]
+            }
+        }
+        
+        if (currMax == 1){
+            return 0
+        }
+        
+        
+        var componentArray = Array(mapNumOccurences.keys)
+        var maxNumArray = [Int]()
+        
+        for num in componentArray{
+            if(mapNumOccurences[num] == currMax){
+                maxNumArray.append(num)
+            }
+        }
+        
+        var degree = getSmallestArray(maxNumArray: maxNumArray, currMax: currMax, inputArr: arr)
+
+        
+        print(degree)
+        return degree
+        
+        
+    }
+    
+    
+    func getSmallestArray(maxNumArray:[Int],currMax: Int , inputArr: [Int])-> Int{
+        var leastSubArrayLenght = 0
+        for i  in 0...maxNumArray.count - 1 {
+        
+            var startIndex = -1
+            var endIndex = -1
+            var numCount = 0
+            var subArrayLenght = 0
+            var currMaxNum = maxNumArray[i]
+            for j in 0...inputArr.count - 1{
+                if(inputArr[j] == currMaxNum){
+                    numCount = numCount + 1
+                    if(startIndex == -1){
+                        startIndex = j
+                    }else if( numCount == currMax){
+                        endIndex = j
+                        subArrayLenght =  endIndex -  startIndex + 1
+                        if(i == 0){leastSubArrayLenght = subArrayLenght }
+                       else if( subArrayLenght < leastSubArrayLenght ){
+                            leastSubArrayLenght = subArrayLenght
+                        }
+                    }
+                }
+            
+        }
+        
+        }
+        return leastSubArrayLenght
+    }
+ */
 
 }
 
